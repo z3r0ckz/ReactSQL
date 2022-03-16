1707cc3d15678674eeaffb488c8b858579880919
Luis Martinez <luismf.1492@gmail.com>
2022-03-15 22:28:49 -0600

    first commit

---
+++ dbconfig.js
@@ -0,0 +1,14 @@
+const config = {
+    user: "ZEROCKZHUAWEI/Zerockz",
+    password: "chofis125",
+    server: "ZEROCKZHUAWEI",
+    database: "test",
+    options: {
+      trustedconnection: true,
+      enableArithAbort: true,
+      instancename: "",
+    },
+    port: 1433
+  };
+   
+  module.exports = config;
